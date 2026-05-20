e6d0357a15169e75d2a8c14e6435e93871e728fb
artem <artx52135@gmail.com>
2026-05-21 01:29:57 +0300

    add visualisation in gazebo

---
+++ build/ackermann_vehicle_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/artem/vechicle/src/ros_gazebo_car/ackermann_vehicle_description
+# Build directory: /home/artem/vechicle/src/ros_gazebo_car/build/ackermann_vehicle_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
